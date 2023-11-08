91656eaba14dea23637f427f371b6a797e243c4e
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-11-08 12:02:06 +0530

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,11 +2,18 @@ import { getData } from "@/actions/getData";
 
 const Sidebar = async () => {
   const data = await getData();
+  const breedsArray = Object.keys(data.message);
 
   return (
-    <div className="h-full overflow-y-scroll gap-2 text-md">
-      {Object.keys(data.message).map((breed) => (
-        <p key={breed}>{breed}</p>
+    <div className="h-full overflow-y-scroll flex flex-col text-lg basis-2/12  bg-[#d7d4ca] text-black">
+      {/* <SearchBar /> */}
+      {breedsArray.map((breed) => (
+        <div
+          key={breed}
+          className="p-4 border-b-2 border-t-2 border-t-[#fffbee] border-b-[#b2afa6]"
+        >
+          <p>{breed}</p>
+        </div>
       ))}
     </div>
   );
