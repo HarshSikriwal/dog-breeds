7d5a46a33ef134923f20cee62927b85362fd43a3
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-11-13 14:36:43 +0530

    Image issue done

--- app/getBreedDetails.ts
+++ app/getBreedDetails.ts
@@ -1,44 +1,40 @@
 export async function getBreedRandomImages(breed?: string, subbreed?: string) {
-  if (subbreed != undefined) {
-    const randomImage = await fetch(
-      `https://dog.ceo/api/breed/${breed}/${subbreed}/images/random`
-    );
+  const apiUrls = subbreed
+    ? [
+        `https://dog.ceo/api/breed/${breed}/${subbreed}/images/random`,
+        `https://dog.ceo/api/breed/${breed}/images/random`,
+        `https://dog.ceo/api/breeds/image/random`,
+      ]
+    : [
+        `https://dog.ceo/api/breed/${breed}/images/random`,
+        `https://dog.ceo/api/breeds/image/random`,
+      ];
 
-    // return randomImage.json();
-    if (!randomImage.ok) {
-      const breedImage = await fetch(
-        `https://dog.ceo/api/breed/${breed}/images/random`
-      );
-      const data = await breedImage.json();
-      // console.log(data);
-      return data.message;
+  for (let api of apiUrls) {
+    console.log(api);
+    try {
+      const response = await fetch(api);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const dataRes = await fetch(data.message);
+      const contentType = dataRes.headers.get("content-type");
+      console.log(contentType);
+      // Check if the content type indicates an image (you might need to customize this)
+      if (contentType && contentType.startsWith("image/")) {
+        try {
+          return data.message;
+        } catch (error) {
+          console.error(`Error parsing JSON: ${error}`);
+          throw new Error("Invalid JSON format");
+        }
+      } else {
+        throw new Error("Invalid image content");
+      }
+    } catch (error) {
+      console.error(`Error fetching dog image: ${error}`);
     }
-    const data = await randomImage.json();
-    return data.message;
   }
-  const randomImage = await fetch(
-    `https://dog.ceo/api/breed/${breed}/images/random`
-  );
-  const data = await randomImage.json();
-  return data.message;
 }
-
-//       const randomImage = await fetch(
-//         `https://dog.ceo/api/breeds/image/random`
-//       );
-//       return randomImage.json();
-//     }
-//   } else {
-//     try {
-//       const randomImage = await fetch(
-//         `https://dog.ceo/api/breed/${breed}/${subbreed}/images/random`
-//       );
-//       return randomImage.json;
-//     } catch (error) {
-//       const randomImage = await fetch(
-//         `https://dog.ceo/api/breeds/image/random`
-//       );
-//       return randomImage.json();
-//     }
-//   }
-// }
