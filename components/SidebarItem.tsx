a790ade4f61677d821bba743fd8235c00f60d4a1
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-11-16 12:59:15 +0530

    Done

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -5,11 +5,12 @@ import React from "react";
 const SidebarItem = ({ breed }: { breed: string }) => {
   const pathname = usePathname();
   const active = breed === pathname.slice(1);
+  console.log(active);
 
   return (
     <div
       className={`p-4  drop-shadow-2xl  border-b-2 border-t-2 border-b-[#b2afa6] bg-[#d7d4ca] ${
-        active && "drop-shadow-none shadow-none bg-[#b2afa6] border-none"
+        active && "drop-shadow-none shadow-none bg-[#9e9d96] border-none"
       }`}
     >
       {breed}
